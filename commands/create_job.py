39f787a269641ccaca35e3e6e81f93b3829d6d4b
Muhammad Tauqir <mtahmed@uwaterloo.ca>
2013-04-15 23:38:16 -0400

    Refine create_job.py and make it easier to use by specifiying path instead of module name.

--- commands/create_job.py
+++ commands/create_job.py
@@ -5,6 +5,7 @@ import importlib
 import time
 import sys
 import os
+import shutil
 
 # Set environment variable.
 sys.path.append(os.getcwd())
@@ -13,7 +14,13 @@ sys.path.append(os.getcwd())
 import messenger
 from job import Job, Splitter, Combiner
 
-def enqueue_job(jobfile):
+def enqueue_job(jobpath):
+    # If the jobpath is not the same is jobs/basename(jobpath), then copy the
+    # file to jobs/ .
+    jobfile = os.path.basename(jobpath)
+    if not os.path.samefile(jobpath,
+                            os.path.join('jobs', jobfile)):
+        shutil.copy(jobpath, 'jobs/')
     # Bind to some other port. Not to the main 33310.
     m = messenger.Messenger(port=33311)
     my_hostname = socket.gethostname()
@@ -21,7 +28,8 @@ def enqueue_job(jobfile):
                            ('0.0.0.0', 33310))
     # This file contains at most 3 methods: split, combine, processor and 2
     # variables: input_data, input_file
-    jobcode = importlib.import_module('jobs.%s' % jobfile)
+    job_module_name = jobfile[:-3]
+    jobcode = importlib.import_module('jobs.%s' % job_module_name)
     try:
         combiner = Combiner()
         combiner.combine = jobcode.combine
@@ -54,9 +62,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='As a master node, enqueue a '
                                                  'new job to be processed by '
                                                  'this system.')
-    parser.add_argument('--jobfile',
+    parser.add_argument('--jobpath', '-j',
                         type=str,
                         help='the path to the file describing the job')
 
     args = parser.parse_args()
-    enqueue_job(args.jobfile)
+    enqueue_job(args.jobpath)
