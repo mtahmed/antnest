7aab059fe450c0513effcdf29fa64d337f483535
Muhammad Tauqir <mtahmed@uwaterloo.ca>
2013-10-10 14:45:32 -0400

    Add a new class Serializable.
    This is a base class from which other classes must inherit to make them
    serializable. This class provides two main methods: serialize() and
    deserialize(...) which return a serialized string representation of the object
    and the object from it's serialized string representation respectively.
    
    This class provides "true" serialization and is able to serialize dynamically
    constructed objects. In other words, it serializes attributes added to live
    objects and also serializes methods etc. and reconstructs the complete live
    object.

--- serialize.py
+++ serialize.py
@@ -1,4 +1,5 @@
 # Standard imports
+import types
 import inspect
 import json
 
@@ -7,6 +8,168 @@ import taskunit
 import job
 import message
 
+
+class Serializable(object):
+    '''
+    This is a base class which provides the basic serialization methods to be
+    used from within the class derived from this class.
+    '''
+    def __init__(self,
+                 noserialize=['__init__', 'noserialize', 'serialize_method',
+                              'serialize', 'deserialize', 'get_vars',
+                              'get_methods']):
+        # List of methods that are not to be serialized.
+        self.noserialize = noserialize
+        return
+
+    def get_vars(self):
+        '''Get all the variable attributes of this class.
+
+        :returns: A dictionary of variable name to value.
+        :rtype: dict
+        '''
+        return {var: getattr(self, var) for var in dir(self)
+                if not inspect.ismethod(getattr(self, var)) and
+                not var.startswith('__') and
+                not var in self.noserialize}
+
+    def get_methods(self):
+        '''Get all the method attributes of this class.
+
+        NOTE: This only returns true methods, bound to the class. To add a bound
+        method to an object, look at ``types.MethodType``.
+
+        :returns: A dictionary of method name to method (function type).
+        :rtype: dict
+        '''
+        return {name: method for name, method in
+                inspect.getmembers(self, predicate=callable)
+                if name not in self.noserialize}
+
+    def serialize_method(self, method):
+        ''' Serialize a method.
+
+        This function also normalizes the method source code:
+
+        1. Removes all leading whitespace from all lines such that the ``def``
+           line of the method starts at column 1.
+        2. Strips any leading or trailing whitespace.
+
+        :param method: The method to be serialized.
+        :type method: A function object.
+        :returns: A string for the source code of the ``method``.
+        :rtype: str
+        '''
+        source = inspect.getsource(method)
+        # Now remove all white space at the start of each line such that
+        # the indentation of the code is maintained and there's no whitespace
+        # before the "def ..." on the first line.
+        whitespace = source.find('def')
+        source = '\n'.join([line[whitespace:] for line in source.split('\n')])
+        source = source.strip()
+
+        return source
+
+    def serialize(self, include_attrs=[], exclude_attrs=[]):
+        '''Serialize this object.
+
+        If both include_attrs and exclude_attrs is empty, all the attributes of
+        the object are serialized.
+        If include_attrs is non-empty, only the attributes in include_attrs
+        are serialized.
+        If exclude_attrs is non-empty, all attributes except the ones in
+        exclude_attrs are serialized.
+        If include_attrs and exclude_attrs overlap, the behaviour is undefined.
+
+        :param include_attrs: A list of names of attributes to serialize.
+        :type include_attrs: list
+        :param exclude_attrs: A list of names of attributes to NOT serialize.
+        :type exclude_attrs: list
+        :returns: A serialized representation of this object.
+        :rtype: str
+        '''
+        all_vars = self.get_vars()
+        all_methods = self.get_methods()
+        if not include_attrs and not exclude_attrs:
+            serialize_vars = all_vars
+            serialize_methods = all_methods
+        elif include_attrs:
+            serialize_vars = {var: value for
+                              var, value in all_vars
+                              if var in include_attrs}
+            serialize_methods = {name: method for
+                                 name, method in all_vars
+                                 if name in include_attrs}
+        elif exclude_attrs:
+            serialize_vars = {var: value for
+                              var, value in all_vars
+                              if var not in exclude_attrs}
+            serialize_methods = {name: method for
+                                 name, method in all_vars
+                                 if name not in exclude_attrs}
+
+        # Attribute dictionary.
+        attr_dict = {}
+        for var, value in serialize_vars.items():
+            attr_dict[var] = json.dumps(value)
+        for name, method in serialize_methods.items():
+            attr_dict[name] = self.serialize_method(method)
+
+        # Finally, dump the json for the whole dict.
+        # We also need the name of the class as the key to the main attributes
+        # dictionary in the final json string.
+        serialized = json.dumps(attr_dict)
+
+        return serialized
+
+    @classmethod
+    def deserialize(cls, serialized):
+        '''Deserialize the ``serialized`` string.
+
+        Note that this method relies on getting the mandatory arguments to
+        ``__init__`` as in the serialized string. So there are two options to
+        use this method:
+
+        1. Don't have any mandatory arguments to ``__init__``.
+        2. Use the same name for attributes as for mandatory arguments (e.g.
+           if a arg1 is a mandatory argument, then the object must have arg1
+           as one of its attributes in the serialized string so it can be passed
+           to ``__init__`` when deserializing and initializing the object.
+
+        :param cls: The class to which to deserialize the string to.
+        :type cls: class
+        :param serialized: The serialized string to be deserialized.
+        :returns: An instance of ``cls`` representing the ``serialized`` string.
+        :rtype: instance of ``cls``
+        '''
+        # Get the list of mandatory arguments to initialize this class.
+        argspec = inspect.getargspec(cls.__init__)
+        num_optional_args = len(argspec.defaults)
+        args = argspec.args
+        mandatory_args = args[1:len(args) - num_optional_args]
+        # Now convert the serialized json string into its python representation.
+        serialized = json.loads(serialized)
+        # Execute any functions so that they are defined in local scope.
+        # All functions/methods start with 'def ' string.
+        for key, val in serialized.items():
+            if val.startswith('def '):
+                exec(val, globals())
+        mandatory_args_vals = [serialized[arg] for arg in mandatory_args]
+        # Now initialize the object with the mandatory arguments.
+        print(mandatory_args)
+        deserialized = cls(*mandatory_args_vals)
+        # Now add all the attributes that aren't mandatory arguments to __init__
+        # to the deserialized object.
+        for key, val in serialized.items():
+            if key in mandatory_args:
+                continue
+            # If it's a method, we need to make a bound method to deserialized.
+            if val.startswith('def '):
+                val = types.MethodType(globals()[key], deserialized)
+            # Now add the attribute to the deserialized object.
+            setattr(deserialized, key, val)
+
+        return deserialized
 
 class Serializer(object):
     '''
